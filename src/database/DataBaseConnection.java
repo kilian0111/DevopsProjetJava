864e86c9d2b5bfa4d1d378120f91abf4b071890a
kilian marmilliot <kilian.marmilliot@gmail.com>
2021-11-24 09:40:10 +0100

    Add package database + connection

---
+++ src/database/DataBaseConnection.java
@@ -0,0 +1,38 @@
+package database;
+
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Properties;
+import java.sql.Connection;
+
+public class DataBaseConnection {
+
+    private Connection connection;
+
+
+    public DataBaseConnection() {
+
+        Properties props = new Properties();
+        try(FileInputStream conf = new FileInputStream("conf.properties")){
+            props.load(conf);
+            Class.forName(props.getProperty("jdbc.driver.class"));
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        String url = props.getProperty("jdbc.url");
+        String login = props.getProperty("jdbc.login");
+        String password = props.getProperty("jdbc.password");
+
+        try{
+        this.connection = DriverManager.getConnection(url,login,password);
+        }catch(Exception e){
+            e.printStackTrace();
+            this.connection = null;
+        }
+    }
+}
