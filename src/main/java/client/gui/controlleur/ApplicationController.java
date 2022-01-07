c060575275f33643dc9d65249f5be965cb8f32fe
p2111000 <jordan.esteves@etu.univ-lyon1.fr>
2022-01-07 11:25:14 +0100

    Ajout de messages à l'affichage

--- src/main/java/client/gui/controlleur/ApplicationController.java
+++ src/main/java/client/gui/controlleur/ApplicationController.java
@@ -1,9 +1,13 @@
 package main.java.client.gui.controlleur;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import main.java.client.Client;
 import main.java.client.MainGui;
+import main.java.common.Message;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,8 +17,11 @@ public class ApplicationController implements Initializable,Icontrolleur {
     private Client client;
     private MainGui mainGui;
 
+    @FXML
+    private TextFlow messagesList;
+
     public void sendMessage(ActionEvent event) {
-        System.out.println("ZKOFPIQPGKI");
+        addMessage(new Message(0, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ultricies vitae mi vitae imperdiet. Proin a fringilla justo. Sed eu lectus a felis varius iaculis vel faucibus risus. Fusce hendrerit fringilla elit ac porttitor. Cras iaculis, urna ut euismod mollis, lectus metus congue mi, at malesuada metus nunc at libero. Curabitur ac sem eget quam volutpat iaculis a vel leo. Nunc bibendum turpis lorem, eu blandit massa ultrices sed. Aenean vel quam lacus. Vivamus diam arcu, laoreet vitae diam quis, tempor egestas nulla"));
     }
 
     @Override
@@ -40,5 +47,20 @@ public class ApplicationController implements Initializable,Icontrolleur {
     @Override
     public void setMainGui(MainGui mainGui) {
         this.mainGui = mainGui;
+    }
+
+    private void addMessage(Message message) {
+        String userName = message.getSender() + "";
+        if (client.getUser().getId() == message.getSender()) {
+            userName = "Moi";
+        }
+
+        Text name = new Text(userName + "\n");
+        name.getStyleClass().add("userName");
+        Text userMessage = new Text(message.getContent() + "\n\n");
+
+        messagesList.getChildren().add(name);
+        messagesList.getChildren().add(userMessage);
+
     }
 }
