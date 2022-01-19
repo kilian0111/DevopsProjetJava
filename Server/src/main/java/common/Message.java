119fe9a6fd113703ac31f01f145556f8d946b930
kilian marmilliot <kilian.marmilliot@gmail.com>
2022-01-19 11:07:22 +0100

    object message conversation + repo

--- Server/src/main/java/common/Message.java
+++ Server/src/main/java/common/Message.java
@@ -1,40 +1,52 @@
 package main.java.common;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.io.Serial;
 import java.io.Serializable;
+import java.util.Date;
 
+@Entity
+@Table(name = "T_MESSAGE")
 public class Message implements Serializable {
-    
-    private int sender;
-    private String content;
-    
-    public static final long serialVersionUID = 12345678902L;
 
-    public Message(int sender, String content) {
-        this.sender = sender;
-        this.content = content;
-    }
+    private Long id;
+    private String Content;
+    private Date dateMessage;
+    private Boolean visible;
+    private Long utilisateurSenderId;
+    private Long conversationId;
 
-    public int getSender() {
-        return sender;
-    }
+    @Serial
+    private  static  final  long serialVersionUID =  1350083881346723535L;
 
-    public void setSender(int sender) {
-        this.sender = sender;
-    }
+    @GenericGenerator(name = "generator", strategy = "increment")
+    @Id
+    @GeneratedValue(generator = "generator")
+    @Column(name = "ME_ID",updatable = false, nullable = false)
+    public Long getId() {return id;}
+    public void setId(Long id) {this.id = id;}
 
-    public String getContent() {
-        return content;
-    }
+    @Column(name = "ME_CONTENT")
+    public String getContent() {return Content;}
+    public void setContent(String content) {Content = content;}
 
-    public void setContent(String content) {
-        this.content = content;
-    }
+    @Column(name = "ME_DT")
+    public Date getDateMessage() {return dateMessage;}
+    public void setDateMessage(Date dateMessage) {this.dateMessage = dateMessage;}
 
-    @Override
-    public String toString() {
-        return "Message{" +
-                "sender='" + sender + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
+    @Column(name = "ME_VISIBLE")
+    public Boolean getVisible() {return visible;}
+    public void setVisible(Boolean visible) {this.visible = visible;}
+
+    @Column(name = "UT_ID")
+    public Long getUtilisateurSenderId() {return utilisateurSenderId;}
+    public void setUtilisateurSenderId(Long utilisateurSenderId) {this.utilisateurSenderId = utilisateurSenderId;}
+
+    @Column(name = "CO_ID")
+    public Long getConversationId() {return conversationId;}
+    public void setConversationId(Long conversationId) {this.conversationId = conversationId;}
 }
+
+
